725fc3db873f31e627c96478838eb59d041a91ca
1Zyke <loronf19@gmail.com>
2022-04-02 01:06:38 -0400

    make it better

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+const path = require('path');
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+const port = process.env.PORT || 5000;
 
-app.listen(5000);
+if (process.env.NODE_ENV !== 'production') {
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    })
+}
+
+app.listen(port, (err) => {
+    if (err) return console.log(err);
+    console.log('Server running on port ', port);
+})
